7d58d80cfb07f463de5c599a98f460ecc6ff5738
heir <71329348+cumcell@users.noreply.github.com>
2020-09-18 15:29:24 +0200

    Update role.js

--- commands/misc/role.js
+++ commands/misc/role.js
@@ -1,14 +1,9 @@
 module.exports.run = (client, message, args) => {
-
-    let role = message.guild.roles.find(role => role.name.startsWith(args[0])) || message.guild.roles.get(args[0]) || message.guild.roles.find(role => role.name === args.join(" ")) || message.mentions.roles.first()
-
-    if (!role) return message.reply('I could not find that role')
-
-    let user = message.mentions.users.first() 
-
-    if (!user) return message.channel.send('I could not find that user!')
-
-    message.guild.member(user).addRole(role)
+let role = message.guild.roles.find(role => role.name.startsWith(args[0])) || message.guild.roles.get(args[0]) || message.guild.roles.find(role => role.name === args.join(" ")) || message.mentions.roles.first()
+if (!role) return message.reply("Cant find that role")
+let user = message.mentions.users.first() 
+if (!user) return message.channel.send('Cant find that user.')
+message.guild.member(user).addRole(role)
     message.channel.send('Added ' + role.toString() + ' to ' + user.tag)
 }
 module.exports.help = {
