57de08d90b841bb2bf686242cc1454bfa8f57f3c
heir <71329348+cumcell@users.noreply.github.com>
2020-09-18 15:06:37 +0200

    Update message.js

--- readcommands/message.js
+++ readcommands/message.js
@@ -4,7 +4,6 @@ const config = require('../config.json')
 module.exports = (client, message) => {
     const args = message.content.slice(config.prefix.length).trim().split(/ +/g);
     const cmd = args.shift().toLowerCase();
-    if (!config.allowOtherPeople && message.author.id !== client.user.id) return;
     if (message.content.indexOf(config.prefix) !== 0) return;
     if (!client.commands.get(cmd)) return;
     client.commands.get(cmd).run(client, message, args,);
