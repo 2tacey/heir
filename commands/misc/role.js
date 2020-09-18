613c5881a99d75b7fe8274c769588fb99022ee21
heir <71329348+cumcell@users.noreply.github.com>
2020-09-18 15:27:23 +0200

    Create role.js

---
+++ commands/misc/role.js
@@ -0,0 +1,17 @@
+module.exports.run = (client, message, args) => {
+
+    let role = message.guild.roles.find(role => role.name.startsWith(args[0])) || message.guild.roles.get(args[0]) || message.guild.roles.find(role => role.name === args.join(" ")) || message.mentions.roles.first()
+
+    if (!role) return message.reply('I could not find that role')
+
+    let user = message.mentions.users.first() 
+
+    if (!user) return message.channel.send('I could not find that user!')
+
+    message.guild.member(user).addRole(role)
+    message.channel.send('Added ' + role.toString() + ' to ' + user.tag)
+}
+module.exports.help = {
+    name:"role",
+    group: "misc",
+}
